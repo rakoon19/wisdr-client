df7e0525cc65140df0da4aa8c8d37d0e4c6aaeaa
rakoon19 <rahatakondo18@gmail.com>
2026-07-13 06:10:05 +0600

    Update auth-client.js

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -4,7 +4,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
     
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000" || `${process.env.BETTER_AUTH_URL}`,
+    baseURL: `${process.env.BETTER_AUTH_URL}`,
     plugins: [
     inferAdditionalFields({
     user: {
